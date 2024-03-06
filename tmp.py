2fccf4392ebc3cc891f6e1b93109da28a1b0a31a
fesvhtr <kyuushi94@outlook.com>
2024-03-06 14:04:34 +0800

    Update data_loader.py

--- tmp.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-
-with open('/home/dh/pythonProject/DocMSU/data/release/docmsu_all.json') as f:
-    data = json.load(f)
-for k, v in data.items():
-    print(k, v)
-    break
